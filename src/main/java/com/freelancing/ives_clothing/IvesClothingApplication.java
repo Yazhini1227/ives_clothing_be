96ea957de6d9a6292f7ce6b49ca8e87262cac215
Yazhini1227 <yazhinimadhu122000@gmail.com>
2025-06-17 11:21:44 +0530

    Initial commit

---
+++ src/main/java/com/freelancing/ives_clothing/IvesClothingApplication.java
@@ -0,0 +1,13 @@
+package com.freelancing.ives_clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvesClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvesClothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancing/ives_clothing/IvesClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancing.ives_clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvesClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
